6f967597bdcdcb673e0c3633c585c852c1f8bae1
ryan johnson <ryan@mobiledatasolutions.net>
2013-10-21 18:14:48 -0500

    Initial Commit

---
+++ week1Tests/week1Tests.m
@@ -0,0 +1,34 @@
+//
+//  week1Tests.m
+//  week1Tests
+//
+//  Created by Ryan Johnson on 10/21/13.
+//  Copyright (c) 2013 nhcc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface week1Tests : XCTestCase
+
+@end
+
+@implementation week1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
